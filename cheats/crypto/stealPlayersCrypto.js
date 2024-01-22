733a7751591450ac233742f5b81b9a25328ebac6
Trexfly3 <127251757+Trexfly3@users.noreply.github.com>
2024-01-22 09:16:44 -0500

    Update stealPlayersCrypto.js

--- cheats/crypto/stealPlayersCrypto.js
+++ cheats/crypto/stealPlayersCrypto.js
@@ -18,7 +18,7 @@
         document.body.append(i);
         window.prompt = i.contentWindow.prompt.bind(window);
         i.remove();
-        let target = prompt("Who's crypto would you like to steal?");
+        let target = prompt("Which person is a Goofy ah monkey");
         let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner;
         stateNode.props.liveGameController.getDatabaseVal("c", (players) => {
             if (players && Object.keys(players).map(x => x.toLowerCase()).includes(target.toLowerCase())) {
